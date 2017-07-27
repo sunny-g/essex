3447eb15d43b2b07e92603458e163b23bfc5be50
Sunny G <sunny.gonna@gmail.com>
2017-07-27 10:07:13 -0700

    initial commit

---
+++ lib/essex.ex
@@ -0,0 +1,18 @@
+defmodule Essex do
+  @moduledoc """
+  Documentation for Essex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Essex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/essex_test.exs
@@ -0,0 +1,8 @@
+defmodule EssexTest do
+  use ExUnit.Case
+  doctest Essex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
